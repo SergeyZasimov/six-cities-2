fe282cbc9a3809559305dc4309120200ae27531a
SergeyZasimov <zasimov.ser@yandex.ru>
2022-09-07 19:36:47 +0300

    Раскрашен вывод в консоль команды help

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,19 +1,28 @@
+import chalk from 'chalk';
 import { CliCommandInterface } from './cli-command.interface.js';
 
 export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
 
   public execute(): void {
-    console.log(`
-    Программа для подготовки данных для REST API сервера.
+    console.log(
+      chalk.underline(' Программа для подготовки данных для REST API сервера.'),
+    );
 
+    console.log(
+      chalk.green(`
     Пример:
         main.js --<command> [--arguments]
+    `),
+    );
 
+    console.log(
+      chalk.italic(`
     Команды:
         --version:                   # выводит номер версии
         --help:                      # печатает этот текст
         --import <path>:             # импортирует данные из TSV
-    `);
+    `),
+    );
   }
 }
