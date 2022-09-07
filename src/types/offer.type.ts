44d3c8c37aab05726c8c73537f28fcc2edc7a55e
SergeyZasimov <zasimov.ser@yandex.ru>
2022-09-07 13:38:50 +0300

    Дополнительные удобства вынесены в отдельное перечисление

--- src/types/offer.type.ts
+++ src/types/offer.type.ts
@@ -1,4 +1,5 @@
 import { City } from './city.type.js';
+import { FeatureType } from './feature-type.enum.js';
 import { HousingType } from './housing-type.enum.js';
 import { Location } from './location.type.js';
 import { User } from './user.type.js';
@@ -17,7 +18,7 @@ export type Offer = {
   rooms: number;
   guests: number;
   price: number;
-  features: string[];
+  features: FeatureType;
   host: User;
   commentsAmount: number;
   location: Location;
