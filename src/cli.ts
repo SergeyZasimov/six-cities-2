1f30dbd87fce4d84b001a3e525e29fee08c698f6
SergeyZasimov <zasimov.ser@yandex.ru>
2022-09-07 19:08:25 +0300

    Создан класс приложения для командной строки и точка запуска приложения

---
+++ src/cli.ts
@@ -0,0 +1,13 @@
+import CliApplication from './app/cli-application.js';
+import HelpCommand from './cli-command/help-command.js';
+import ImportCommand from './cli-command/import -command.js';
+import VersionCommand from './cli-command/version-command.js';
+
+const cliManager = new CliApplication();
+cliManager.registerCommand([
+  new HelpCommand(),
+  new VersionCommand(),
+  new ImportCommand(),
+]);
+
+cliManager.processCommand(process.argv);
