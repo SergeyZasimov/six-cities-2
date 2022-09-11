bf96e2590872172b7edaef1fd506a816a7a99ffa
SergeyZasimov <zasimov.ser@yandex.ru>
2022-09-11 12:54:37 +0300

    Описан тип объекта, получаемый от JSON-server

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,14 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previewImages: string[];
+  photos: string[];
+  housingTypes: string[];
+  features: string[];
+  users: string[];
+  emails: string[];
+  passwords: string[];
+  avatars: string[];
+  userTypes: string[];
+};
