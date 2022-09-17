5754ad7a883a1a67a1f894563b327c15fd8c6fda
SergeyZasimov <zasimov.ser@yandex.ru>
2022-09-17 19:01:40 +0300

    Подготовлен контейнер с описанием зависимостей

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import LoggerService from './services/logger/logger.service.js';
 import Application from './app/application.js';
 import ConfigService from './services/config/config.service.js';
+import { Container } from 'inversify';
+import { Component } from './types/component.types.js';
+import { LoggerInterface } from './services/logger/logger.interface.js';
+import { ConfigInterface } from './services/config/config.interface.js';
 
+const appContainer = new Container();
+appContainer.bind<Application>(Component.Application).to(Application);
+appContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
+appContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
 
-const logger = new LoggerService();
-const config = new ConfigService(logger);
-
-const app = new Application(logger, config);
+const app = appContainer.get<Application>(Component.Application);
 await app.init();
