9c45ac8be79d855642818defd197ce011ddd0df2
SergeyZasimov <zasimov.ser@yandex.ru>
2022-10-05 14:47:40 +0300

    Добавлена обработка HttpError в ExceptionFilter.

--- src/services/errors/exception-filter.ts
+++ src/services/errors/exception-filter.ts
@@ -4,6 +4,8 @@ import { Component } from '../../types/component.types.js';
 import { LoggerInterface } from '../logger/logger.interface.js';
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import HttpError from './http-error.js';
+import { createErrorObject } from '../../utils/create-error-object.js';
 
 @injectable()
 export default class ExceptionFilter implements ExceptionFilterInterface {
@@ -13,8 +15,21 @@ export default class ExceptionFilter implements ExceptionFilterInterface {
     this.logger.info('Register ExceptionFilter');
   }
 
-  public catch( error: Error, _req: Request, res: Response, _next: NextFunction ) {
+  private handleHttpError( error: HttpError, _req: Request, res: Response, _next: NextFunction ) {
     this.logger.error(error.message);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message });
+    res.status(error.httpStatusCode).json(createErrorObject(error.message));
+  }
+
+  private handleOtherError( error: Error, _req: Request, res: Response, _next: NextFunction ) {
+    this.logger.error(error.message);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(createErrorObject(error.message));
+  }
+
+  public catch( error: Error | HttpError, req: Request, res: Response, next: NextFunction ) {
+    if (error instanceof HttpError) {
+      this.handleHttpError(error, req, res, next);
+      return;
+    }
+    this.handleOtherError(error, req, res, next);
   }
 }
